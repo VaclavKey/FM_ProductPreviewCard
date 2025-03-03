89836fbe642adb6cb9b30dee9b8dc85342451f56
Vaclav Valentine <152690177+VaclavKey@users.noreply.github.com>
2025-03-04 00:10:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
+"use strict";
 let img = document.querySelector(".card-image");
-
 function updateImageSource() {
     if (window.innerWidth <= 768) {
         img.src = "images/image-product-mobile.jpg";
-    } else {
+    }
+    else {
         img.src = "images/image-product-desktop.jpg";
     }
 }
-
 updateImageSource();
-
 window.addEventListener("resize", updateImageSource);
